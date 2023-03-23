d789f1b8c583e8f514e72eb5a7a0d1aeed1cca49
Yang Xianglin <yxlyzxl@gmail.com>
2023-03-23 18:11:22 +0800

    models

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+
+from models.badnet import BadNet
+from models.resnet import resnet18, resnet34, resnet50, ResNet
+
+__all__ = [
+    "ResNet",
+    "resnet18",
+    "resnet34",
+    "resnet50",
+    "BadNet",
+]
